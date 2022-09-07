1145217818a3162004d219bfee15c6cc1c49f3f4
jootang2 <joohwan.song15b@gmail.com>
2022-09-07 11:44:49 +0900

    기본 function

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+
+}
 
 func main() {
-	name := "jootang2"
-	name = "Joo"
-	fmt.Println(name)
+	repeatMe("jootang", "hwan", "John", "Angel")
 }
