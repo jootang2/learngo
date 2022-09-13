0fade186af636bd21aa9c00cecc31630b69fd061
jootang2 <joohwan.song15b@gmail.com>
2022-09-13 16:35:56 +0900

    for, range

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func multiply(a, b int) int {
 	return a * b
 }
 
-func lenAndUpper(name string) (lenght int, uppercase string) {
-	defer fmt.Println("I'm done")
-	lenght = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func repeatMe(words ...string) {
@@ -23,6 +23,6 @@ func repeatMe(words ...string) {
 
 // test
 func main() {
-	totalLenght, up := lenAndUpper("joohwan")
-	fmt.Println(totalLenght, up)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
