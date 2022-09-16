ba605abd7f2524ad2d3acdb646da09c053f87e8d
jootang2 <joohwan.song15b@gmail.com>
2022-09-16 13:34:13 +0900

    array & slice

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import "fmt"
 
 // test
 func main() {
-	a := 2         //a 주소에 담긴 값 : 2
-	b := &a        //b : a 주소 값
-	*b = 4         //*b : b 값 (a 주소 값)을 4로 변경 => a 주소에 담긴 값 4로 변경
-	fmt.Println(a) // => 출력 : 4
+	names := []string{"juhwan", "john", "jootang2"}
+	names = append(names,"angel")
+	fmt.Println(names)
 }
