cc6e36246a61a0c8da1376ca6e7885a3839b7744
jootang2 <joohwan.song15b@gmail.com>
2022-09-16 13:46:19 +0900

    Maps

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 // test
 func main() {
-	names := []string{"juhwan", "john", "jootang2"}
-	names = append(names,"angel")
-	fmt.Println(names)
+	jootang2 := map[string]string{"name": "jootang2" , "age" : "27"}
+	for _,value := range jootang2 {
+		fmt.Println(value)
+	}
 }
