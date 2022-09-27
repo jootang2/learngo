3b409b410c6a1185f796e7a40e6dab80b62e29d8
jootang2 <joohwan.song15b@gmail.com>
2022-09-27 11:45:39 +0900

    hitURL _ URL CHECKER

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/learngo/mydict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	baseword := "hello"
-	dictionary.Add(baseword, "First")
-	dictionary.Search(baseword)
-	dictionary.Delete(baseword)
-	word, err := dictionary.Search(baseword)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
-	}
+var errRequestFailed = errors.New("Request Failed")
 
+func main() {
+	urls := []string{"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
