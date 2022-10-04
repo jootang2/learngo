8dc344f98dbad06f8db16e3c5fde0e14c758128f
jootang2 <joohwan.song15b@gmail.com>
2022-10-04 10:23:37 +0900

    Go routines

--- main.go
+++ main.go
@@ -3,42 +3,30 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
-	var results = make(map[string]string)
-	urls := []string{"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results{
-		fmt.Println(url, result)
-	}
+	go Count("jootang2 said")
+	go Count("Mingji said")
+	time.Sleep(time.Second * 5)
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+// func hitURL(url string) error {
+// 	fmt.Println("Checking", url)
+// 	resp, err := http.Get(url)
+// 	if err != nil || resp.StatusCode >= 400 {
+// 		fmt.Println(err, resp.StatusCode)
+// 		return errRequestFailed
+// 	}
+// 	return nil
+// }
+
+func Count(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "this is golang", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
