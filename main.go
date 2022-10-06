26debba9985bc55ec6d835db91464d3b314d0c25
jootang2 <joohwan.song15b@gmail.com>
2022-10-06 13:17:12 +0900

    getPages 1

--- main.go
+++ main.go
@@ -1,49 +1,43 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type requestResult struct {
-	url string
-	status string
-}
-
-var errRequestFailed = errors.New("Request Failed")
+var baseURL string = "https://jootang2.tistory.com/"
 
 func main() {
-	results := make(map[string]string)
-	c := make(chan requestResult)
-	urls := []string{"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-	for i:=0;i < len(urls); i ++{
-		result := <- c
-		results[result.url] = result.status
-	}
+	getPages()
+}
 
-	for url, status := range results {
-		fmt.Println(url, status)
+func getPages() int {
+	res, err := http.Get(baseURL)	
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+	
+	
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
 
-func hitURL(url string, c chan <- requestResult) {
-	resp, err := http.Get(url)
-	status := "OK"
-	if err != nil || resp.StatusCode >= 400 {
-		status = "FAILED"
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode)
 	}
-	c <- requestResult{url:url, status: status}
 }
