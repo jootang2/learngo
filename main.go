9808cbbb5a49438f916eb77cb350cbddb98aa135
jootang2 <joohwan.song15b@gmail.com>
2022-10-11 11:33:56 +0900

    page에 해당되는 URL 가져오기

--- main.go
+++ main.go
@@ -4,18 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://jootang2.tistory.com/"
+var baseURL string = "https://monzee.tistory.com/"
 
 func main() {
-	getPages()
+	totalPages := getPages()
+
+	for i := 1; i <= totalPages; i++ {
+		getPage(i)
+	}
 }
 
+func getPage(page int) {
+	pageURL := baseURL + "?page=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageURL)
+}
 func getPages() int {
-	res, err := http.Get(baseURL)	
+	pages := 0
+	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
 
@@ -24,10 +34,11 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	fmt.Println(doc)
-	
-	
-	return 0
+	doc.Find(".area-paging").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length() - 2
+	})
+
+	return pages
 }
 
 func checkErr(err error) {
