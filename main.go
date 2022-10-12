6680c9175305132184e0e8dc638042e4cd84215d
jootang2 <joohwan.song15b@gmail.com>
2022-10-12 10:53:00 +0900

    title, summary, date 가져오기

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedMonzee struct {
+	id	string
+	title	string
+	summary string
+	date string
+}
+
 var baseURL string = "https://monzee.tistory.com/"
 
 func main() {
@@ -22,6 +29,29 @@ func main() {
 func getPage(page int) {
 	pageURL := baseURL + "?page=" + strconv.Itoa(page)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".article-content")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("href")
+		fmt.Println(id)
+		title := card.Find(".title").Text()
+		fmt.Println(title)
+		summary := card.Find(".summary").Text()
+		fmt.Println(summary)
+		date := card.Find(".date").Text()
+		fmt.Println(date)
+
+	})
+
 }
 func getPages() int {
 	pages := 0
